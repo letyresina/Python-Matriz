44aba5914ef23c5622d52f541f2706939f02e399
letyresina <leticiaresina@gmail.com>
2023-09-15 11:47:46 -0300

    Subindo exercício 3 (não finalizado)

--- exercicios/exercicio3.py
+++ exercicios/exercicio3.py
@@ -4,9 +4,20 @@
     os elementos da matriz que sejam maiores do que 100 pelo valor zero e exiba a matriz novamente.
 '''
 
+matriz = []
+
 def exibeMatriz(matriz): # função para formatar matriz
     for linha in matriz:
         for item in linha:
             print(item, end='\t')
         print()
 
+for i in range(15):
+    for linha in range(3):
+        num = int(input("Informe um número inteiro: "))
+        lista = []
+        for coluna in range(5):
+            lista.append(num)
+        matriz.append(lista)
+
+exibeMatriz(matriz)
